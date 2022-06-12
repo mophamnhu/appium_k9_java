996356eaf58223cff0143706c0394641999471e6
Mo Pham <mophamnhu@gmail.com>
2022-06-13 01:21:35 +0700

    Lab_06

--- src/lab_06/ElementURL.java
+++ src/lab_06/ElementURL.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class ElementURL {
 
     public static void main(String[] args) {
-        System.out.print("Input an URL: ");
+        System.out.print("Input an URL to check HTTP or HTTPS: ");
         Scanner inputURL = new Scanner(System.in);
         String url = inputURL.nextLine();
 
-//String url = "https://google.com";
-//Check http OR https; domain name, .com OR .net
+        System.out.println("URL Protocol: " + url.substring(0, url.indexOf(":")));
+        System.out.println("URL domain name: " + url.substring((url.indexOf("//")+2), url.indexOf(".")));
+        System.out.println("Domain: " + url.substring(url.indexOf(".")));
 
     }
 }

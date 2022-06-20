e18d701677d6d9cc28b11095794ea6c41cf3e29a
Mo Pham <mophamnhu@gmail.com>
2022-06-20 23:37:01 +0700

    Lab_7_2

--- src/lab_07/MainClass.java
+++ src/lab_07/MainClass.java
@@ -2,13 +2,11 @@ package lab_07;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class MainClass extends TotalSalaryEmployees{
-
+public class MainClass{
     public static void main(String[] args) {
 
         List<Employee> employeeList = new ArrayList<>();
-        int salaryTotal = 0;
+        int employeeId = 1;
 
         Employee new_Employee_1 = new FullTimeEmployee();
         Employee new_Employee_2 = new FullTimeEmployee();
@@ -22,8 +20,18 @@ public class MainClass extends TotalSalaryEmployees{
         employeeList.add(new_Employee_4 );
         employeeList.add(new_Employee_5 );
 
-        TotalSalaryEmployees finalSalary = new TotalSalaryEmployees();
-        System.out.println("total salary of 5 employees: "+ finalSalary.calTotalSalary(employeeList));
+        System.out.println("Salary of employees:");
+        for (Employee employee : employeeList) {
+            System.out.println("Employee: " + employeeId + "  " +  "$" + employee.getSalary());
+            ++employeeId ;
+        }
+        System.out.println("Total salary of employee in the list is:  $" + MainClass.CalTotalSalary(employeeList));
     }
-
+    public static int CalTotalSalary (List<Employee> employeeList) {
+        int totalSalary = 0;
+        for (Employee index : employeeList) {
+            totalSalary = totalSalary + index.getSalary();
+        }
+        return totalSalary;
+    }
 }
